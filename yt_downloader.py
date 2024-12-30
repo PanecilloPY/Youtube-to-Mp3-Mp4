a2d7e984488bd0cac0fdf8a636e14b2a1e33f865
Mxiim <94921139+PanecilloPY@users.noreply.github.com>
2024-12-30 22:10:18 +0100

    Update yt_downloader.py
    Mejora de estilo tkinter

--- yt_downloader.py
+++ yt_downloader.py
@@ -2,6 +2,7 @@ import os
 import yt_dlp
 import tkinter as tk
 from tkinter import filedialog, messagebox
+from tkinter import ttk 
 
 def download_video(url, format_choice, download_folder):
     ydl_opts = {}
@@ -49,39 +50,56 @@ def start_download():
 
     download_video(url, format_choice, folder)
 
-# Configuración de la interfaz gráfica
+# Config
 root = tk.Tk()
 root.title("YouTube Downloader")
+root.geometry("600x200")  
+root.resizable(False, False)  
+root.config(bg="#f7f7f7") 
 
-frame = tk.Frame(root, padx=10, pady=10)
-frame.pack(padx=10, pady=10)
+# Font
+font_style = ('Helvetica Neue', 12)
 
-# Campo de URL
-url_label = tk.Label(frame, text="URL del video de YouTube:")
-url_label.grid(row=0, column=0, sticky="w")
-url_entry = tk.Entry(frame, width=50)
+frame = tk.Frame(root, padx=10, pady=10, bg="#f7f7f7")
+frame.pack(padx=15, pady=10, fill="both", expand=True)  
+
+# Reajuste de la ventana
+frame.grid_columnconfigure(0, weight=0)  
+frame.grid_columnconfigure(1, weight=1)  
+frame.grid_columnconfigure(2, weight=0)  
+
+frame.grid_rowconfigure(0, weight=0) 
+frame.grid_rowconfigure(1, weight=0) 
+frame.grid_rowconfigure(2, weight=0)  
+frame.grid_rowconfigure(3, weight=0)  
+
+# URL
+url_label = tk.Label(frame, text="URL del video de YouTube:", font=font_style, bg="#f7f7f7")
+url_label.grid(row=0, column=0, sticky="w", pady=3)
+url_entry = ttk.Entry(frame, width=40, font=font_style)
 url_entry.grid(row=0, column=1, padx=5, pady=5)
 
-# Opciones de formato
-format_label = tk.Label(frame, text="Formato de descarga:")
-format_label.grid(row=1, column=0, sticky="w")
-format_var = tk.StringVar(value="mp4")
-mp4_radio = tk.Radiobutton(frame, text="MP4", variable=format_var, value="mp4")
-mp4_radio.grid(row=1, column=1, sticky="w")
-mp3_radio = tk.Radiobutton(frame, text="MP3", variable=format_var, value="mp3")
-mp3_radio.grid(row=2, column=1, sticky="w")
+# Opciones de formato en menú desplegable
+format_label = tk.Label(frame, text="Formato de descarga:", font=font_style, bg="#f7f7f7")
+format_label.grid(row=1, column=0, sticky="w", pady=3)
 
-# Selección de carpeta de destino
-folder_label = tk.Label(frame, text="Carpeta de destino:")
-folder_label.grid(row=3, column=0, sticky="w")
+format_var = tk.StringVar(value="mp4")  # Valor por defecto
+format_options = ["mp4", "mp3"]  # Opciones de formato
+format_menu = ttk.OptionMenu(frame, format_var, *format_options)
+format_menu.grid(row=1, column=1, padx=5, pady=5, sticky="w")
+
+# Carpeta de destino
+folder_label = tk.Label(frame, text="Carpeta de destino:", font=font_style, bg="#f7f7f7")
+folder_label.grid(row=2, column=0, sticky="w", pady=3)
 download_folder = tk.StringVar()
-folder_entry = tk.Entry(frame, textvariable=download_folder, width=40)
-folder_entry.grid(row=3, column=1, padx=5, pady=5, sticky="w")
+folder_entry = ttk.Entry(frame, textvariable=download_folder, width=35, font=font_style)
+folder_entry.grid(row=2, column=1, padx=5, pady=5, sticky="w")
+
 browse_button = tk.Button(frame, text="Examinar", command=browse_folder)
-browse_button.grid(row=3, column=2, padx=5, pady=5)
+browse_button.grid(row=2, column=2, padx=5, pady=5)
 
 # Botón de descarga
-download_button = tk.Button(frame, text="Descargar", command=start_download)
-download_button.grid(row=4, column=1, pady=10)
+download_button = tk.Button(frame, text="Descargar", command=start_download, relief="flat", bg="#34b7f1", fg="white", font=font_style)
+download_button.grid(row=3, column=1, pady=10)
 
 root.mainloop()
